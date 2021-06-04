367eab4e698990f2afefda530e91bf07dea744c4
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-04 21:53:48 +0100

    added user model

---
+++ src/models/user.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+      unique: true,
+    },
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      unique: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+const options = {
+  sequelize,
+  modelName: "user",
+  timestamps: true,
+  underscored: true,
+  freezeTableName: true,
+};
+
+User.init(schema, options);
+
+module.exports = User;
