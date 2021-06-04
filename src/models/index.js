4f97ae031e6b2635fb8d1801820b177e92574b2c
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-04 22:10:11 +0100

    set up model associations

---
+++ src/models/index.js
@@ -0,0 +1,29 @@
+const Comment = require("./comment");
+const Post = require("./post");
+const User = require("./user");
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
