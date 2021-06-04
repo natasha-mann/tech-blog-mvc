ab50a572571d1067192d036a9e929cf4a99c82f5
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-04 21:57:14 +0100

    added post model

---
+++ src/models/post.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  body: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "user",
+      key: "id",
+    },
+  },
+};
+
+const options = {
+  sequelize,
+  modelName: "post",
+  timestamps: true,
+  underscored: true,
+  freezeTableName: true,
+};
+
+Post.init(schema, options);
+
+module.exports = Post;
