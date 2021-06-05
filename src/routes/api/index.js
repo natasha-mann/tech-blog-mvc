33b2e91dcdfc6ff38450a243a9f758f5b24638c3
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-05 12:05:24 +0100

    commented out unused routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 
-const userRoutes = require("./users");
-const commentRoutes = require("./comments.js");
+// const userRoutes = require("./users");
+// const commentRoutes = require("./comments");
 const blogPostRoutes = require("./blog-posts");
 
 const router = Router();
 
-router.use("/users", userRoutes);
-router.use("/comments", commentRoutes);
+// router.use("/users", userRoutes);
+// router.use("/comments", commentRoutes);
 router.use("/posts", blogPostRoutes);
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 
 const apiRoutes = require("./api");
-const htmlRoutes = require("./html");
+// const htmlRoutes = require("./html");
 
 const router = Router();
 
 router.use("/api", apiRoutes);
-router.use("/", htmlRoutes);
+// router.use("/", htmlRoutes);
 
 module.exports = router;
