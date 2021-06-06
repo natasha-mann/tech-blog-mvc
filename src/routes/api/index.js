ada9e2e832d5aaaa1f401ab2b096894a79b5af16
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-06 11:38:27 +0100

    deleted files

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,13 +1,9 @@
 const { Router } = require("express");
 
-// const userRoutes = require("./users");
-// const commentRoutes = require("./comments");
 const blogPostRoutes = require("./blog-posts");
 
 const router = Router();
 
-// router.use("/users", userRoutes);
-// router.use("/comments", commentRoutes);
 router.use("/posts", blogPostRoutes);
 
 module.exports = router;
