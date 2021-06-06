c64b0d79b9e2762a1e1ee2e2f53d1be7758f1fb4
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-06 21:32:50 +0100

    added user routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const blogPostRoutes = require("./blog-posts");
+const usersRoutes = require("./users");
 
 const router = Router();
 
 router.use("/posts", blogPostRoutes);
+router.use("/users", usersRoutes);
 
 module.exports = router;
