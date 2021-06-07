25cca9159e313ad72c3935c8d90d4c1e4187a4e1
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-07 22:44:40 +0100

    updated home to show private nav if logged in

--- src/controllers/html/public.js
+++ src/controllers/html/public.js
@@ -2,13 +2,14 @@ const { Post, User, Comment } = require("../../models");
 
 const renderHome = async (req, res) => {
   try {
+    const { userId } = req.session;
     const postData = await Post.findAll({
       include: [{ model: User }, { model: Comment }],
     });
     const posts = postData.map((post) => {
       return post.get({ plain: true });
     });
-    res.render("home", { posts });
+    res.render("home", { userId, posts });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ error: "Failed to render" });
