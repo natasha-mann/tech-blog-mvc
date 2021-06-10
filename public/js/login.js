239fcbcf1d75f2dc8e082e0aa26266d0365397ad
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-10 17:56:56 +0100

    added client side alert on incorrect password

--- public/js/login.js
+++ public/js/login.js
@@ -12,6 +12,14 @@ const login = async (userDetails) => {
 
   if (response.status !== 200) {
     console.error("Failed login");
+    $("#alert-div").empty();
+    $("#alert-div")
+      .append(`<div id="error-alert" class="alert alert-warning d-flex align-items-center" role="alert">
+    <i class="fas fa-exclamation-triangle me-4"></i>
+    <div>
+      Email or password incorrect. Please try again!
+    </div>
+  </div>`);
   } else {
     window.location.replace("/dashboard");
   }
